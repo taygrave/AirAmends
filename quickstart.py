eb97e77a65a819187c3f089585f4b3a69e47024a
Taylor Hesselgrave <tkhesselgrave@gmail.com>
2015-02-16 18:52:47 -0800

    Got gmail API to connect to my own account, search my emails on a defined query, return emails ids, then get email objects from that list

--- quickstart.py
+++ quickstart.py
@@ -1,6 +1,7 @@
 import httplib2
 
 from apiclient.discovery import build
+from apiclient import errors
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run
@@ -31,10 +32,68 @@ http = credentials.authorize(http)
 # Build the Gmail service from discovery
 gmail_service = build('gmail', 'v1', http=http)
 
-# Retrieve a page of threads
-threads = gmail_service.users().threads().list(userId='me').execute()
+def query_messages(service, user_id, query=''):
+  """List all Messages of the user's mailbox matching the query.
 
-# Print ID for each thread
-if threads['threads']:
-  for thread in threads['threads']:
-    print 'Thread ID: %s' % (thread['id'])
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    query: String used to filter messages returned.
+
+  Returns:
+    List of Messages that match the criteria of the query. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate ID to get the details of a Message.
+  """
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               q=query).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id, q=query,
+                                         pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+def get_message(service, user_id, msg_id):
+  """Get a Message with given ID.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: The ID of the Message required.
+
+  Returns:
+    A Message.
+  """
+  try:
+    message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+
+    print 'Message snippet: %s' % message['snippet']
+
+    return message
+
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+def testing():
+    #added 'southwest' to query to start with managable list of results
+    query1 = "itinerary, confirmation, flight, number, departure, taxes, southwest"
+    msg_list = query_messages(gmail_service,"me", query1)
+    msg_obj_list = []
+
+    for i in range(len(msg_list)):
+        msg_id = msg_list[i]['id']
+        msg_obj = get_message(gmail_service,'me', msg_id)
+        msg_obj_list.append(msg_obj)
+
+    return msg_obj_list
