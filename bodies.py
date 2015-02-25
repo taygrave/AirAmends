aa1ecd62853a00e51f9006c5aa271629f92f81e7
Tay <thistlegrave@gmail.com>
2015-02-25 14:49:26 -0800

    found the winning regex! it is: r"\(([A-Z]{3})\)|\(([A-Z]{3}) |\>([A-Z]{3})\<|;([A-Z]{3})\&")

--- bodies.py
+++ bodies.py
@@ -8,34 +8,39 @@ def find_airports():
     list_aircodes = [airport.id for airport in all_airports]
 
 #>>> # r"(a|b|c)"
-    aircode_str = r"[\(;>]([A-Z]{3})[\)&<]"
+    # aircode_str = r"[\(;>]([A-Z]{3})[\)&<]"
+    p = re.compile(r"\(([A-Z]{3})\)|\(([A-Z]{3}) |\>([A-Z]{3})\<|;([A-Z]{3})\&")
 
-    # aircode_str = r"(SFO|DEN)"  
-    # aircode_str = r"\(([A-Z]{3}) | \>([A-Z]{3})\<"
-    # |;([A-Z]{3})\&"
-    # aircode_str = r"\>([A-Z]{3})\<"
-    # p = re.compile(r"\(([A-Z]{3}) | \>([A-Z]{3})\<")
-    # p = re.compile(r"((SFO)?|(JFK)?)")
-    #tried taking out parenthesis and it was a madhouse, need parenthesis.
-    # aircode_str = r"\(([A-Z]{3})"
-    # re1 = (A-Z
-    # re2 = >A-Z<
-    # re3 = ;A-Z&
-    
     msg_list = s.query(model.Email).all()
     for msg_obj in msg_list:
         list_refinds = p.findall(msg_obj.body)
+        x = []
 
-        #list comprehension to ensure three letter findings are airport codes
-        list_airfinds = [item for item in list_refinds if item in list_aircodes]
+        for tup4 in list_refinds:
+            z = [c for c in tup4 if c != '']
+            x.extend(z)
+        
+            # for item in tup4:
+            #     if item != "":
+            #         x.append(item)
+            #         print x
+
+        # list comprehension to ensure three letter findings are airport codes
+        list_airfinds = [item for item in x if item in list_aircodes]
 
         # print "*" * 20
         # print "message id: %d" %msg_obj.id
         # print msg_obj.date
         # print msg_obj.sender 
         # print msg_obj.subject
-        print len(list_airfinds), ":", list_airfinds
+        print msg_obj.id, ":", len(list_airfinds), ":", list_airfinds
 
+
+def test_Regex():
+    my_text = "Depart (SFO) arrive >DEN<, stop in (SLC), return >SFO<."
+    aircodes = (r"\(([A-Z]{3})\)|\>([A-Z]{3})\<")
+    results = re.findall(aircodes, my_text)
+    print results
 
 def print_to_file():
     """Queries the database for the body of the messages and prints out content into txt files so we can examine them ourselves """
@@ -52,3 +57,4 @@ def print_to_file():
 
 # print_to_file()
 find_airports()
+# test_Regex()
