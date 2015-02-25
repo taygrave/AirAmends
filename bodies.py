35d8d1198d405c300417a7bbd889b352ea88a1ec
Tay <thistlegrave@gmail.com>
2015-02-24 22:21:57 -0800

    refined regex search, moved base64 extra decoding to the body of the quickstart.py file so that bodies.py find_airports() starts with one clean input, moved email message object stuff into quickstart.py as well

--- bodies.py
+++ bodies.py
@@ -1,7 +1,5 @@
 import model
 import re
-import base64
-import email
 
 def airport_codes():
     """Returns a list of all airport codes in db"""
@@ -14,45 +12,33 @@ def airport_codes():
 
     return list_aircodes
 
-#TODO set variable for confirmation code numbers 
-
 def query_emails():
     """Connects to db and returns list of all email message objects"""
     s = model.connect()
     msg_list = s.query(model.Email).all()
     return msg_list
 
+#TODO set variable for confirmation code numbers 
+
 def find_airports():
     """Pulls email bodies (strings) from msg objs (list) and parses them to return a list of airport codes per message"""
+    #FIXME: This funciton should receive just ONE well-defined input (an email body ready to search)
     list_aircodes = airport_codes()
     msg_list = query_emails()
-    #tried taking out parenthesis and it was a madhouse, need parenthesis.
-    # aircode_str = r"\(([A-Z]{3})"
 
     aircode_str = r"[\(;>]([A-Z]{3})[\)&<]"
-
+    #tried taking out parenthesis and it was a madhouse, need parenthesis.
+    # aircode_str = r"\(([A-Z]{3})"
+    # aircode_str = r"\b[A-Z]{3}\b"
     # re1 = (A-Z
     # re2 = >A-Z<
     # re3 = ;A-Z&
-
 
     for msg_obj in msg_list:
         list_refinds = re.findall(aircode_str, msg_obj.body)
 
         #list comprehension to ensure three letter findings are airport codes
         list_airfinds = [item for item in list_refinds if item in list_aircodes]
-
-        if list_airfinds == []:
-            decoded_str = extra_decode(msg_obj)
-            #could put a test or something here? is it possible that decoded_str could be None?
-            list_refinds = re.findall(aircode_str, decoded_str)
-            list_airfinds = [item for item in list_refinds if item in list_aircodes]
-            
-            # #re-writes the body files so that the coded illegible ones are replaced with decoded body
-            # filename = "body/" + str(msg_obj.id) + ".txt"
-            # f = open(filename, 'w')
-            # print >> f, decoded_str
-            # f.close   
 
         # print "*" * 20
         # print "message id: %d" %msg_obj.id
@@ -62,27 +48,16 @@ def find_airports():
         print list_airfinds
 
 
-def extra_decode(msg_obj):
-    """Decodes message bodies from a message object from the database that is still encrypted and returns decoded string of body"""
-    msg = email.message_from_string(msg_obj.body)
-    for part in msg.walk():
-        msg.get_payload()
-        if part.get_content_type() == 'text/html' or part.get_content_type() == 'text/plain':
-                decoded_str = base64.urlsafe_b64decode(part.get_payload().encode('UTF-8'))
-                return decoded_str
-
 def print_to_file():
     """Queries the database for the body of the messages and prints out content into txt files so we can examine them ourselves """
+    #TODO: DELETE this function once you're done!
     msg_list = query_emails()
-
     for msg in msg_list:
-        body = msg.body
+        body = msg.body.encode('utf-8')
         filename = "body/" + str(msg.id) + ".txt"
         f = open(filename, 'w')
         print >> f, body
         f.close
-
-
 
 # print_to_file()
 find_airports()
--- quickstart.py
+++ quickstart.py
@@ -2,6 +2,7 @@ import httplib2
 import base64
 import model
 import email
+import pdb
 
 from apiclient.discovery import build
 from apiclient import errors
@@ -9,24 +10,12 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run
 
-#FIXME: clean this UP! 
-#FIXME: this process takes so long, way to speed it up? 
-#TODO: actually add the current user to the Users table (w/ all info) and input user_id as actual user_id
-
 #exmaple provided by: https://developers.google.com/gmail/api/quickstart/quickstart-python
-
-# Path to the client_secret.json file downloaded from the Developer Console
-CLIENT_SECRET_FILE = 'client_secret.json'
-
-# Check https://developers.google.com/gmail/api/auth/scopes for all available scopes
+CLIENT_SECRET_FILE = 'client_secret.json' # Path to the client_secret.json file downloaded from the Developer Console
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/gmail.readonly'
-
-# Location of the current user's credentials storage file
-# change this to keep in same place as the client_secret_file??
-STORAGE = Storage('gmail.storage')
+STORAGE = Storage('gmail.storage') #Location of the current user's credentials storage file
 
 # Start the OAuth flow to retrieve credentials
-# flow is likely an object 
 flow = flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=OAUTH_SCOPE)
 http = httplib2.Http()
 
@@ -38,71 +27,62 @@ if credentials is None or credentials.invalid:
 # Authorize the httplib2.Http object with our credentials
 http = credentials.authorize(http)
 
-# Build the Gmail service from discovery
+# Build the Gmail service from discovery - this is the part that takes very long
 gmail_service = build('gmail', 'v1', http=http)
+print "completed: service build"
 
-def query_messages(service, user_id, query=''):
-  """List all Messages of the user's mailbox matching the query.
+def query_messages(service, user_id, query):
+  """Returns list of ids of all user's messages matching a query.
 
   Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    query: String used to filter messages returned.
-
-  Returns:
-    List of Messages that match the criteria of the query. Note that the
-    returned list contains Message IDs, you must use get with the
-    appropriate ID to get the details of a Message.
+    service: Authorized Gmail API service (instance).
+    user_id: User's email address / special value "me" for authenticated user (string).
+    query: (string).
   """
+  list_msg_ids = [] #will stay empty if no messages match the query
+
   try:
-    response = service.users().messages().list(userId=user_id,
-                                               q=query).execute()
-    #TODO: rename variables accordingly
-    messages = []
-
-    #TODO Handle if there are no messages matching the query (somewhere) or just put in doc string
+    response = service.users().messages().list(userId=user_id, q=query).execute()
+    
     if 'messages' in response:
-      messages.extend(response['messages'])
-
-    #this part allows you to get complete results, basically gets EVERYTHING matching query
-    while 'nextPageToken' in response:
+      list_msg_ids.extend(response['messages'])
+    
+    while 'nextPageToken' in response: #this part allows you to get complete results, basically gets EVERYTHING matching query (usually not reached, needs >60 finds)
       page_token = response['nextPageToken']
-      response = service.users().messages().list(userId=user_id, q=query,
-                                         pageToken=page_token).execute()
-      messages.extend(response['messages'])
+      response = service.users().messages().list(userId=user_id, q=query, pageToken=page_token).execute()
+      list_msg_ids.extend(response['messages'])
 
-    return messages
+    print "completed: got query msg list"
+    return list_msg_ids
+  
   except errors.HttpError, error:
     print 'An error occurred: %s' % error
 
 def get_message(service, user_id, msg_id):
-  """Get a Message with given ID.
+  """Get a Message with given ID and return it as a decoded string.
 
   Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    msg_id: The ID of the Message required.
-
-  Returns:
-    A Message, decoded.
+    service: Authorized Gmail API service (instance).
+    user_id: User's email address / special value "me" for authenticated user (string).
+    msg_id: ID of the Message to retrieve (string).
   """
   try:
     message = service.users().messages().get(userId=user_id, id=msg_id, format="raw").execute()
+    print "completed: retrieved message"
 
-    #FIXME: United emails aren't being decoded for some reason
     str_raw_msg = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
-
+    print "completed: decoded message"
+    
     return str_raw_msg
 
   except errors.HttpError, error:
     print 'An error occurred: %s' % error
 
 def add_msgs_to_db():
-
+  #TODO = add docstring
     query1 = "itinerary, confirmation, flight, number, departure, taxes from:-me subject:-fwd subject:-re subject:-fw subject:-check"
     msg_list = query_messages(gmail_service,"me", query1)
+    print "completed: query messages"
     s = model.connect()
 
     for item in msg_list:
@@ -113,17 +93,26 @@ def add_msgs_to_db():
         if msg_id == msg_thrd_id:
 
           str_raw_msg = get_message(gmail_service,'me', msg_id)
-          msg_str1 = email.message_from_string(str_raw_msg)
+          email_msg = email.message_from_string(str_raw_msg)
 
-          msg_date = msg_str1['Date']
-          msg_sender = msg_str1['From']
-          msg_subject = msg_str1['Subject']
+          msg_date = email_msg['Date']
+          msg_sender = email_msg['From']
+          msg_subject = email_msg['Subject']
+          msg_body = str_raw_msg
+          # msg_encoding = email_msg['Content-Transfer-Encoding']
+          
+          #checks if msg body is still encoded and decodes if necessary (multipart messages don't return a content-transer-encoding)
+          for part in email_msg.walk():
+            if (part.get_content_type() == 'text/html' or part.get_content_type() == 'text/plain') and part['Content-Transfer-Encoding'] == 'base64':
+              decoded_str = base64.urlsafe_b64decode(part.get_payload().encode('UTF-8'))
+              msg_body = decoded_str.decode('utf-8')
 
-          # and make sure isn't in db already
+          #checks if email with same sender & subject is in db already to prevent adding duplicates in case airline sent duplicates
           exists = s.query(model.Email).filter(model.Email.sender == msg_sender, model.Email.subject == msg_subject).first()
 
+          #adds to db if is a unique itinerary confimation
           if exists == None:
-            entry = model.Email(user_id=1, msg_id=msg_id, date=msg_date, sender=msg_sender, subject=msg_subject, body=str_raw_msg)
+            entry = model.Email(user_id=1, msg_id=msg_id, date=msg_date, sender=msg_sender, subject=msg_subject, body=msg_body)
             s.add(entry)
             s.commit() 
 
