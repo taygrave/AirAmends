42e206acf0bd79c6294913e7b438ccac8f87467c
Tay <thistlegrave@gmail.com>
2015-03-15 15:35:21 -0700

    addressing issue if no messages found matching query, modified gmailapiworks add_msgs_to_db to return differently depending if messages found or not

--- gmailapiworks.py
+++ gmailapiworks.py
@@ -43,39 +43,45 @@ def add_msgs_to_db(service, user_id, query=query):
   """Sets query and adds unique, parsed, and extra decoded if necessary, message components to the db """
   service = service
   msg_list = query_messages(service, query)
-  print "completed: queried gmail messages, adding to db..."
+  
+  print "completed: queried gmail messages..."
   print msg_list
-  s = model.connect()
+  
+  if msg_list == []:
+    return "No messages found matching query."
 
-  # check if message is unique, parse, perhaps decode, and add to db
-  for item in msg_list:
-    #only take those where msg_id = thread_id to ensure its the root email
-      if item['id'] == item['threadId']: 
-        #pull actual message from gmail api using its id
-        str_raw_msg = get_message(service, item['id'])
-        
-        msg_body = str_raw_msg
-        #and turn it into an email object to begin parsing
-        email_msg = email.message_from_string(str_raw_msg)
-        msg_date = convert_date(email_msg['Date'])
-        msg_sender = email_msg['From']
-        msg_subject = email_msg['Subject']
-        
-        # checks if msg body is still encoded and decodes if necessary
-        for part in email_msg.walk():
-          if (part.get_content_type() == 'text/html' or part.get_content_type() == 'text/plain') and part['Content-Transfer-Encoding'] == 'base64':
-            decoded_str = base64.urlsafe_b64decode(part.get_payload().encode('UTF-8'))
-            msg_body = decoded_str.decode('UTF-8')
+  else: 
+    s = model.connect()
 
-        #ensures only unique itinerary confirmation email message info is added to the db
-        exists = s.query(model.Email).filter(model.Email.sender == msg_sender, model.Email.subject == msg_subject, model.Email.user_id == user_id).first()
-        
-        if exists == None:
-          entry = model.Email(user_id=user_id, msg_id=item['id'], date=msg_date, sender=msg_sender, subject=msg_subject, body=msg_body)
-          s.add(entry)
-          s.commit()
+    # check if message is unique, parse, perhaps decode, and add to db
+    for item in msg_list:
+      #only take those where msg_id = thread_id to ensure its the root email
+        if item['id'] == item['threadId']: 
+          #pull actual message from gmail api using its id
+          str_raw_msg = get_message(service, item['id'])
+          
+          msg_body = str_raw_msg
+          #and turn it into an email object to begin parsing
+          email_msg = email.message_from_string(str_raw_msg)
+          msg_date = convert_date(email_msg['Date'])
+          msg_sender = email_msg['From']
+          msg_subject = email_msg['Subject']
+          
+          # checks if msg body is still encoded and decodes if necessary
+          for part in email_msg.walk():
+            if (part.get_content_type() == 'text/html' or part.get_content_type() == 'text/plain') and part['Content-Transfer-Encoding'] == 'base64':
+              decoded_str = base64.urlsafe_b64decode(part.get_payload().encode('UTF-8'))
+              msg_body = decoded_str.decode('UTF-8')
 
-  print "Successfully added emails to the db"
+          #ensures only unique itinerary confirmation email message info is added to the db
+          exists = s.query(model.Email).filter(model.Email.sender == msg_sender, model.Email.subject == msg_subject, model.Email.user_id == user_id).first()
+          
+          if exists == None:
+            entry = model.Email(user_id=user_id, msg_id=item['id'], date=msg_date, sender=msg_sender, subject=msg_subject, body=msg_body)
+            s.add(entry)
+            s.commit()
+
+    print "Successfully added emails to the db"
 
 def convert_date(date_str):
   """Converts email obj date (string) into a datetime date object."""
