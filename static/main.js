214c30e83bc5db2bb6a9f37a77c197045e5da121
Tay <thistlegrave@gmail.com>
2015-04-07 22:01:50 -0700

    adding comments and chanegd loadMethods to loadModal

--- static/main.js
+++ static/main.js
@@ -1,9 +1,11 @@
 // This script provides JavaScript functionality that is accesible to entire site: does map animation, keeps carbon dashboard updated, supports the modal, calculate table totals, and most importantly changes all a links in the info div to load in place
 
 function loadInfo(path) {
+    //default has already been prevented by the time an a href path is passed to this function (input), will render the response into the info div instead and call setDashboard
       $.get(path, function(response) {
         $("#info").html(response);
         if (path == '/get_flights'){
+            //if on homepage, calls for animation of map
             loadFlights();
         }
         setDashboard();
@@ -11,17 +13,18 @@ function loadInfo(path) {
     }
 
 function loadFlights() {
-    // add in a try catch
+    //queries for user's flight coords json and sets as global variable to be used by drawPairs for map animation
       $.get("/flights.js", function(response) {
         try {
             window.pairs = JSON.parse(response);
-        drawPairs();
+            drawPairs();
         } catch(e) {
         }
       });
     }
 
-function loadMethods(evt) {
+function loadModal(evt) {
+    //pops modal up with correct html page (which must be in /static to work)
     evt.preventDefault();
     $("#modal-wrapper").load("/static/carboncalcs.html", function () {
       $("#terms-modal").modal();
@@ -29,9 +32,8 @@ function loadMethods(evt) {
   }
 
 function setDashboard() {
-    // console.log("setting dashboard");
-    // console.log(carbonDebt);
-    // console.log(typeof(carbonDebt));
+    //updates the carbon dashboard everytime with updated and formatted values for carbon debt, price, and donate button w/ correct send-off value of debt.
+    //called when new page is loaded or flights are added/deleted - converts inputs into proper items for load
     carbonPrice = ('$'+(carbonDebt*CO2ePrice).toFixed(2).toString());
     $("#carbon-debt").html(carbonDebt.toFixed(2));
     $("#carbon-price").html(carbonPrice);
@@ -39,7 +41,7 @@ function setDashboard() {
     }
 
 function flightTotal() {
-    // on getflights.html sums total number of flights in second table column
+    //on getflights.html sums total number of flights in second table column
     var tdFlights = document.getElementsByClassName("flights");
     var fTotal = document.getElementById("fTotal");
     var fSum = 0;
@@ -75,10 +77,12 @@ function addTotals() {
 }
 
 function drawPairs() {
-    // Transform the short [lat,lng] format in our
-    // data into the {x, y} expected by arc.js.
+    //MAPBOX SCRIPT modified (broken out into two funcs)
+    //Source: https://www.mapbox.com/mapbox.js/example/v1.0.0/animating-flight-paths/
+    //Transform the short [lat,lng] format in our data into the {x, y} expected by arc.js.
     
     if (! window.pairs){
+        //catches if there is an error in the json pairs, will not run rest of func
         return;
     }
 
@@ -86,8 +90,7 @@ function drawPairs() {
 
     for (var i = 0; i < window.pairs.length; i++) {
         try {
-            // Transform each pair of coordinates into a pretty
-            // great circle using the Arc.js plugin, as included above.
+            // Transform each pair of coordinates into a pretty great circle using the Arc.js plugin, as included above.
             var generator = new arc.GreatCircle(
                     obj(window.pairs[i][0]),
                     obj(window.pairs[i][1]));
@@ -103,26 +106,20 @@ function drawPairs() {
             .addTo(map);
 
         } catch(e) {
-            // coordinates are bad
+            //if coordinates exist but are bad
             continue;
         }
-        // Leaflet expects [lat,lng] arrays, but a lot of
-        // software does the opposite, including arc.js, so
-        // we flip here.
+        // Leaflet expects [lat,lng] arrays, but a lot of software does the opposite, including arc.js, so we flip here.
         var totalLength = newLine._path.getTotalLength();
         newLine._path.classList.add('path-start');
         // This pair of CSS properties hides the line initially
-        // See http://css-tricks.com/svg-line-animation-works/
-        // for details on this trick.
+        // See http://css-tricks.com/svg-line-animation-works/   for details on this trick.
         newLine._path.style.strokeDashoffset = totalLength;
         newLine._path.style.strokeDasharray = totalLength;
-        // Offset the timeout here: setTimeout makes a function
-        // run after a certain number of milliseconds - in this
-        // case we want each flight path to be staggered a bit.
+        // Offset the timeout here: setTimeout makes a function run after a certain number of milliseconds - in this case we want each flight path to be staggered a bit.
         setTimeout((function(path) {
             return function() {
-                // setting the strokeDashoffset to 0 triggers
-                // the animation.
+                // setting the strokeDashoffset to 0 triggers the animation.
                 path.style.strokeDashoffset = 0;
             };
         })(newLine._path), i * 100);
@@ -130,7 +127,7 @@ function drawPairs() {
 }
 
 $(document).ready(function(){
-    $("#load-methods").on("click", loadMethods);
+    $("#load-methods").on("click", loadModal);
     // Intercept clicks of anchor tags
     $('#info').on('click', 'a', function(ev) {
         var ogHref = ev.target.href;
@@ -139,6 +136,7 @@ $(document).ready(function(){
     });
 
     // MAPBOX SCRIPT
+    // Source: https://www.mapbox.com/mapbox.js/example/v1.0.0/animating-flight-paths/
       L.mapbox.accessToken = 'pk.eyJ1IjoidGF5Z3JhdmUiLCJhIjoiTC02ZVBocyJ9.v07HDiBCNqymCU6IsCF1jQ';
     // This is an advanced example that is compatible with
     // modern browsers and IE9+ - the trick it uses is animation
